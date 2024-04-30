2b99497c37da91b62139bcf97078014ba117d2b8
ali2s2love <ali2s2love@gmail.com>
2024-04-30 18:24:24 +0330

    Edit docker compose for just http

---
+++ Chapter01/Connect To PostgreSQL/Connect To PostgreSQL/Program.cs
@@ -0,0 +1,27 @@
+using Connect_To_PostgreSQL.Data;
+using Connect_To_PostgreSQL.Dto;
+using Connect_To_PostgreSQL.Endpoints;
+using Connect_To_PostgreSQL.Entities;
+using Connect_To_PostgreSQL.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDBContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+     app.ApplyMigration();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MyArticleEndpoints();
+
+app.Run();
